bcfe10cc7d08321ce9944324cf43e8f618498c36
Celes-BTGL <yurika-lover@hotmail.com>
2022-04-24 17:05:11 -0400

    Glitch space

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    drawLines();
     var playing = false,
         artistname = $(".artist-name"),
         musicName = $(".music-name"),
@@ -14,14 +15,14 @@ $(document).ready(function() {
     function load() {
         audioData = [
             {
-                name: "DIE4U",
+                name: "Nihilist Blues",
                 artist: "BMTH",
-                src: "assets/DIE4U.mp3?raw=true"
+                src: "assets/nihilist-blues.mp3?raw=true"
             },
             {
-                name: "Run",
-                artist: "Bring Me The Horizon",
-                src: "assets/Run.mp3?raw=true"
+                name: "Space Dementia",
+                artist: "Muse",
+                src: "assets/space-dementia.mp3?raw=true"
             },
             {
                 name: "Forgotten",
@@ -278,4 +279,34 @@ $(document).ready(function() {
         }
         playSong();
     });
+
+
+
+function getHeight(){
+  return window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+}
+
+function drawLines(){
+  const lines = document.getElementsByClassName('line');
+  if(lines.length) {
+    for (let i = 0; i < lines.length; i++) {
+        document.body.removeChild(lines[i]);
+    }
+  }
+  
+  for(i = 0; i < getHeight()/10; i++){
+    const line = document.createElement("div");  
+    line.className = `line line-${i}`;
+    line.style.top = `${i * 10}px`;
+    const time = Math.random() * 5;
+    line.style.animation = `lines ${time}s infinite`;
+    document.body.appendChild(line) ;
+  }
+}
+
+window.onresize = function(event) {
+  drawLines();
+};
 });
